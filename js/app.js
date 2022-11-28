81f5096aec0ede91fdde36796293366b3eed4f11
davCesur <david.s247064@cesurformacion.com>
2022-11-28 21:43:21 +0100

    Resubida JS después de borrado accidental

--- js/app.js
+++ js/app.js
@@ -39,3 +39,85 @@ window.addEventListener('load', function(){
 
 });
 
+// A PARTIR DE AQUÍ CONTROLAMOS TABLA Y FORMULARIO
+
+$('#addProducto').click(function () {
+    var id = $('#id').val(); 
+    var nombre = $('#nombre').val();
+    var referencia = $('#referencia').val();
+    var categoria = $('#categoria').val();
+    var precio = $('#precio').val();
+    var cantidad = $('#cantidad').val();
+    $('table tbody').append(`
+        <tr>
+            <td id="id">${id}</td>
+            <td id="nombre">${nombre}</td>
+            <td id="referencia">${referencia}</td>
+            <td id="categoria">${categoria}</td>
+            <td id="precio">${precio}</td>
+            <td id="cantidad">${cantidad}</td>
+            <td><button onclick="dobleClick()">Editar</button></td>
+            <td><button class="btn btn-primary btnborrar">Borrar</button></td>
+        </tr>`
+    );
+  
+    document.addEventListener('DOMContentLoaded', function(){
+    let formulario = document.getElementById('formulario');
+    formulario.addEventListener('submit', function() {
+    formulario.reset();
+  });
+  
+  
+  });
+  });
+  
+  // Delegar evento buscando botones por clase
+  $(document).on('click', '.btnborrar', function() {
+    $(event.target).closest('tr').remove();
+  });
+  
+  
+  // creamos un evento doble click para cada una de las celdas de la tabla
+  
+  function dobleClick() {
+    const tds=document.querySelectorAll("td");
+    
+    for(td of tds) {
+        td.addEventListener("dblclick",function() {
+            // creamos un nuevo input con el valor actual de la celda
+            let input=document.createElement('input');
+            input.value=this.textContent;
+    
+            // evento que se ejecuta cuando el input pierde el foco
+            input.addEventListener("blur",function() {
+                removeInput(this);
+            });
+    
+            // evento que se ejecuta cada vez que se deja de pulsar una tecla
+            input.addEventListener("keydown",function(e) {
+    
+                // la tecla 13, es el Enter
+                if(e.which==13) {
+                    removeInput(this);
+                }
+            });
+    
+            // quitamos el contenido de la celda de la tabla
+            this.textContent="";
+    
+            // Ponemos en la celda el input que hemos creado
+            this.appendChild(input);
+        });
+    }
+    
+    // Eliminamos el input y ponemos el valor del mismo
+    function removeInput(e) {
+        e.parentElement.textContent=e.value;
+    }
+  }
+
+  function mostrarFormulario() {
+    let formularrio = document.getElementById("formulario").style.visibility = "visible";
+    let tabla = document.getElementById("tabla_productos").style.marginTop = "5%";
+    let boton = document.getElementById("add").style.visibility = "hidden";
+  }
